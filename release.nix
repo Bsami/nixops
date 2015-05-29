8544d9e3581caf335e265021eaaba6709bbc1397
Sami BOUHLEL <sami.bouhlel@predictix.com>
2015-05-29 11:03:39 +0100

    added x86_64-darwin

--- release.nix
+++ release.nix
@@ -69,7 +69,7 @@ rec {
       '';
   };
 
-  build = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" ] (system:
+  build = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "x86_64-darwin"] (system:
     with import <nixpkgs> { inherit system; };
 
     pythonPackages.buildPythonPackage rec {
